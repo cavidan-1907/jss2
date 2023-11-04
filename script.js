0a3743dad3dac9578ee71027f778ce6477933cd0
Cavi.dan1907 <141852698+cavidan-1907@users.noreply.github.com>
2023-11-04 13:10:30 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 
 const imtahanneticesi=function(a,b){
     let d=a+b;
-    if (b<17){
+    if (b<17 || d<51){
         return "telebe kesildi";
-    }
-    else if(d<51){
-        return "telebe kesildi"
-    }
+    
     else if(d>=51 && d<=60){
         return "E";
     }
