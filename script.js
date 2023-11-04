837ec78a0de8ceacfb8242c94d57ab1f5314fb8d
cavidan-1907 <cavidanvelzade9@gmail.com>
2023-11-04 11:22:33 +0400

    js

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const imtahanneticesi=function(a,b){
+    let d=a+b;
+    if (b<17){
+        return "telebe kesildi";
+    }
+    else if(d<51){
+        return "telebe kesildi"
+    }
+    else if(d>=51 && d<=60){
+        return "E";
+    }
+    else if (d>=61 && d<=70)
+    {
+        return "D";
+    }
+    else if (d>=71 && d<=80)
+    {
+        return "c";
+    }
+    else if (d>=81 && d<=90)
+    {
+        return "b";
+    }
+    else if (d>=91 && d<=100)
+    {
+        return "A";
+    }
+    else {
+        return "telebenin ballarini duzgun daxil edin"
+    }
+
+}
+console.log(imtahanneticesi(34,17));
